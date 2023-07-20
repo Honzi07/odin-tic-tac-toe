fea222af7f8f7413303408693d4f3c964fe118be
Honzi07 <nyisztor.zsolt@gmail.com>
2023-07-20 04:30:16 +0200

    Some bug fixes now the board correctly shows the array content.

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
+'use strict';
+
 const game = (function () {
   const gameCell = document.querySelectorAll('.game-cell');
-  const gameBoard = ['X', 'O', 'X', , , , 'X', 'O'];
+  const gameBoard = ['X', 'O', , , , , 'X', 'O'];
 
-  const renderGameCellContent = function () {
-    gameBoard.forEach((el, i) => (gameCell[i].textContent = el));
-    gameBoard.forEach((el, i) => console.log(el, i));
-  };
+  const addSignToCell = function (player1, player2) {
+    console.log(player1, player2);
 
-  const addSignToCell = function () {
     gameCell.forEach((cell) =>
       cell.addEventListener('click', function (e) {
-        let i = e.target.dataset.index;
+        const i = e.target.dataset.index;
 
         if (!gameBoard[i]) {
-          gameBoard[i] = 'X';
+          gameBoard[i] = player1.sign;
         }
 
         e.target.innerText = gameBoard[i];
-
-        console.log(e.target.dataset);
-        console.dir(e.target);
+        console.log(gameBoard);
       })
     );
   };
 
   console.log(gameBoard);
-  console.log(gameCell);
-  return { renderGameCellContent, addSignToCell, gameCell, gameBoard };
+  return { addSignToCell, gameCell, gameBoard };
 })();
 
 const player = function () {
@@ -43,19 +39,17 @@ const player = function () {
     submitName.addEventListener('submit', function (e) {
       e.preventDefault();
 
-      player1Name = e.currentTarget.player1.value || 'player1';
-      player2Name = e.currentTarget.player2.value || 'player2';
+      const player1Name = e.currentTarget.player1.value || 'player1';
+      const player2Name = e.currentTarget.player2.value || 'player2';
 
       const player1 = createPlayer(player1Name, 'X');
       const player2 = createPlayer(player2Name, 'O');
 
-      console.log(player1);
-      console.log(player2);
+      game.addSignToCell(player1, player2);
     });
   };
-
   return { createPlayer, namePlayer };
 };
 
-const test = player();
-test.namePlayer();
+const initGame = player();
+initGame.namePlayer();
